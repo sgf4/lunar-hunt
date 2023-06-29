c9574238aaa40149589be2319921f47589ab482b
sgf4 <sgf4@users.noreply.github.com>
2023-06-29 18:27:54 +0200

    -

--- src/main.c
+++ src/main.c
@@ -1,5 +1,21 @@
-#include <stdio.h>
+#include <ncurses.h>
 
 int main() {
-    printf("hello\n");
+    initscr();  // Initialize ncurses
+
+    start_color();  // Enable color support
+    curs_set(0);
+    init_pair(1, COLOR_RED, COLOR_BLACK);  // Initialize color pair 1 (red text on black background)
+
+    attron(COLOR_PAIR(1));  // Turn on color pair 1
+    printw("Hello, World!");  // Print the text
+    attroff(COLOR_PAIR(1));  // Turn off color pair 1
+
+    refresh();  // Refresh the screen
+
+    getch();  // Wait for user input
+
+    endwin();   // End ncurses
+
+    return 0;
 }
